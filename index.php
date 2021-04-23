bf1331f1453809e04a281c43dae2628cb5ab45af
Danilo <dyamuez@umariana.edu.co>
2021-04-23 12:26:59 -0500

    se agregaron : a los titulos del formulario

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-md-2">
-            <h4>Identificacion *</h4><br>
-            <h4>Nombre *</h4><br>
-            <h4>Correo *</h4><br>
-            <h4>Telefono *</h4><br>
-            <h4>Tipo *</h4><br>
-            <h4>Mensaje *</h4>
+            <h4>Identificacion: *</h4><br>
+            <h4>Nombre: *</h4><br>
+            <h4>Correo: *</h4><br>
+            <h4>Telefono: *</h4><br>
+            <h4>Tipo: *</h4><br>
+            <h4>Mensaje: *</h4>
 
         </div>
         <div class="col-md-4">
