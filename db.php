adc1722c081d1243a1bc73525d0201949194c6e7
Carlos-code36 <cacharfuelan@umariana.edu.co>
2021-04-24 21:06:01 -0500

    Se modifico la conexion a la base de datos, de local a remota

--- db.php
+++ db.php
@@ -3,9 +3,9 @@
 session_start();
 
 $conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
+    'pqrsunimar.cii4idtkymkd.us-east-1.rds.amazonaws.com',
+    'admin',
+    '123456789',
     'pqrsunimar'
 );
 
